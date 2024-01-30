c439bf3cca0be3a6d4cb0427aeda5861e502f515
Jonas <petersen0108@gmail.com>
2024-01-30 14:25:36 +0100

    Started on table for time

---
+++ src/routes/random/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import svelteTable from "svelte-table"
+    import {faker} from "@faker-js/faker"
+    faker.seed(5);
+
+
+    let sortBy = "id";
+    let iconAsc = "↑";
+    let iconDesc = "↓";
+    let selectedCols = ["id", "first_name", "last_name"];
+    let numRows = 10;
+    let data = [];
+
+
+    let generateData = () => {
+    data = Array(numRows)
+      .fill("")
+      .map((n, i) => {
+        let d = {
+          id: i,
+          first_name: faker.person.firstName(),
+          last_name: faker.person.lastName()
+        };
+        return d;
+      });
+  };
+
+
+</script>
